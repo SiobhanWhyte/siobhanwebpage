7d5425785e3fe61822a6e6731d7c6562c0032ff3
SiobhanWhyte <77633144+SiobhanWhyte@users.noreply.github.com>
2024-08-24 22:28:38 +0100

    replacing edits

---
+++ script.js
@@ -0,0 +1,101 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        // this is a symbol
+        handleSymbol(value);
+    } else {
+        // this is a number
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                // need two numbers to do math
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningTotal.toString();  // Convert runningTotal to a string
+            runningTotal = 0;
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case "+":
+        case "-":
+        case "×":
+        case "÷":
+            handleMath(symbol);
+            break;
+    }
+}
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        // do nothing
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    switch (previousOperator) {
+        case "+":
+            runningTotal += intBuffer;
+            break;
+        case "-":
+            runningTotal -= intBuffer;
+            break;
+        case "×":
+            runningTotal *= intBuffer;
+            break;
+        case "÷":
+            runningTotal /= intBuffer;
+            break;
+    }
+}
+
+function handleNumber(numberString) {
+    if (buffer === "0") {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
+function init() {
+    document.querySelector('.calc-buttons')
+        .addEventListener('click', function (event) {
+            buttonClick(event.target.innerText);
+        });
+}
+
+init();
